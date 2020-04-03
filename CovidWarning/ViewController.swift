9869c1c82d959e9561116755ab491aec3303bc01
Raimundas Sakalauskas <raimis@pinstudios.com>
2020-04-03 17:35:57 +0300

    Initial Commit

---
+++ CovidWarning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CovidWarning
+//
+//  Created by Raimundas Sakalauskas on 2020-04-03.
+//  Copyright © 2020 Raimundas Sakalauskas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
