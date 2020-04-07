22e1c70ac51e57ce7f53f996705a60bb11861f8a
Raimundas Sakalauskas <raimis@pinstudios.com>
2020-04-08 00:02:48 +0300

    add CLBeacon extension method getDistance

---
+++ CovidWarning/Extensions/CLBeacon.swift
@@ -0,0 +1,20 @@
+//
+// Created by Raimundas Sakalauskas on 2020-04-07.
+// Copyright (c) 2020 Raimundas Sakalauskas. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+extension CLBeacon {
+    func getDistance(txPower: Double) -> Double {
+        /*
+         * RSSI = TxPower - 10 * n * lg(d)
+         * n = 2 (in free space)
+         *
+         * d = 10 ^ ((TxPower - RSSI) / (10 * n))
+         */
+
+        pow(10.0, (txPower - Double(self.rssi)) / (10 * 2))
+    }
+}
